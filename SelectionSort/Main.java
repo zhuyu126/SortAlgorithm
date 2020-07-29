9afa79e81728c1a6217eb7dfe37cacadbd4dc72d
robinson <zhuyu553@gmail.com>
2020-07-29 17:07:34 +0800

    SelectionSort Performance Test

--- SelectionSort/Main.java
+++ SelectionSort/Main.java
@@ -4,25 +4,25 @@
  */
 public class Main {
     public static void main(String[] args) {
-//        int[] arr={23,12,34,67,11,75};
-//        System.out.println("排序前为:");
-//        dispay(arr);
-//        SimpleSelectionSort.sort(arr);
-//        System.out.println("排序后为:");
-//        for(int e: arr) {
-//            System.out.print(e + " ");
-//        }
-//        System.out.println();
-//
-//        System.out.println("自定义类的选择排序:");
-//        Student[] students = {new Student("Alice", 98),
-//                new Student("Lee", 100),
-//                new Student("Rey", 66)};
-//        SelectionSort.sort(students);
-//        for(Student student: students) {
-//            System.out.print(student + " ");
-//        }
-//        System.out.println();
+        int[] arr={23,12,34,67,11,75};
+        System.out.println("排序前为:");
+        dispay(arr);
+        SimpleSelectionSort.sort(arr);
+        System.out.println("排序后为:");
+        for(int e: arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+
+        System.out.println("自定义类的选择排序:");
+        Student[] students = {new Student("Alice", 98),
+                new Student("Lee", 100),
+                new Student("Rey", 66)};
+        SelectionSort.sort(students);
+        for(Student student: students) {
+            System.out.print(student + " ");
+        }
+        System.out.println();
 
         System.out.println("选择排序性能分析:");
         int[] dataSize = {10000, 100000};
