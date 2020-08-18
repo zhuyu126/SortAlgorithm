1e04b1fbe826087b031c1a03c9ea321dc2e11865
robinson <zhuyu553@gmail.com>
2020-08-18 17:53:10 +0800

    QuickSort Optimized by Used One Random

--- QuickSort/QuickSortMain.java
+++ QuickSort/QuickSortMain.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
  */
 public class QuickSortMain {
     public static void main(String[] args) {
-//        int n=10000000;
         int[] dataSize={1000000,10000000};
         for (int n:dataSize){
             Integer[]array=ArrayGenerator.generateRandomArray(n,n);
             Integer[] array2= Arrays.copyOf(array,array.length);
-            SortingHelper.sortTest("QuickSort",array);
-            SortingHelper.sortTest("MergeSort",array2);
+            System.out.println("无序数组");
+            SortingHelper.sortTest("MergeSort",array);
+            SortingHelper.sortTest("QuickSort",array2);
+            System.out.println("有序数组");
+            Integer[] array3=ArrayGenerator.generateOrderArray(n);
+            Integer[] array4=Arrays.copyOf(array3,array3.length);
+            SortingHelper.sortTest("MergeSort",array3);
+            SortingHelper.sortTest("QuickSort",array4);
         }
     }
 }
