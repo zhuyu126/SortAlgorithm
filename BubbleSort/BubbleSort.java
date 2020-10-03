bf8f9d249a80dd80a212eae98c0aad8e5479b800
zhuyu <zhuyu553@gmail.com>
2020-10-03 15:16:26 +0800

    BubbleSort Optimized by Element is Order

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -11,6 +11,25 @@ public class BubbleSort {
             }
         }
     }
+
+    public static<E extends Comparable<E>>void bubbleSortOptimized(E[] data){
+        if (data==null||data.length==0){
+            return;
+        }
+        for (int i=0;i<data.length-1;i++){
+            boolean isSwapped=false;
+            for (int j=0;j<data.length-i-1;j++){
+                if (data[j].compareTo(data[j+1])>0){
+                    swap(data,j,j+1);
+                    isSwapped=true;
+                }
+            }
+            if (!isSwapped) {
+                break;
+            }
+        }
+
+    }
     public static <E extends  Comparable<E>>void anotherBubbleSort(E[] data){
         if (data==null||data.length==0){
             return;
