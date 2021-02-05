6f22dc18d16f479628c01e48fa337cc6486634e2
zhuyu <zhuyu553@gmail.com>
2021-02-05 13:02:48 +0800

    CountingSort

--- 08-CountingSort/CountingSort.java
+++ 08-CountingSort/CountingSort.java
@@ -1,2 +1,21 @@
 public class CountingSort {
+    //处理元素取值范围是【0，R）的计数排序
+    public void sort(int[] nums) {
+        int R=3;//R为数据的个数
+        int [] cnt=new int[R];
+        for ( int num:nums){
+            cnt[num]++;
+        }
+        //[index[i],index[i+1])的值为i
+        int[] index=new int[R+1];
+        for (int i=0;i<R;i++){
+            index[i+1]=index[i]+cnt[i];
+        }
+        //[index[i],index[i+1])的值为i
+        for (int i=0;i+1<index.length;i++){
+            for (int j=index[i];j<index[i+1];j++){
+                nums[j]=i;
+            }
+        }
+    }
 }
