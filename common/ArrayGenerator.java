f4f6c46535baa9035610d2ab7bcba71a6163ee59
zhuyu <zhuyu553@gmail.com>
2021-02-24 15:33:43 +0800

    update ArrayGenerator

--- common/ArrayGenerator.java
+++ common/ArrayGenerator.java
@@ -31,4 +31,37 @@ public class ArrayGenerator {
         }
         return arr;
     }
+    /**
+     * 生成一个随机的字符串数组
+     * @param n 数组长度
+     * @param w 字符串长度
+     * @return 数组
+     */
+    public static String[] generateRandomStringArray(int n, int w){
+        // https://www.ascii-code.com/
+        // 33-126 可打印字符
+        String[] arr = new String[n];
+        Random rnd = new Random();
+        for(int i = 0; i < n; i ++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < w; j ++) {
+                sb.append((char)(rnd.nextInt(94) + 33));
+            }
+            arr[i] = sb.toString();
+        }
+        return arr;
+    }
+
+    /**
+     * 数组交换
+     * @param arr
+     * @param i
+     * @param j
+     * @param <E>
+     */
+    private static <E> void swap(E[] arr, int i, int j){
+        E t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
 }
